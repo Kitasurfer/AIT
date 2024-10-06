3fdfe1af7a88623c64d6adea4e59e81c2c113fe0
BogdanF <sovtour@gmail.com>
2024-10-06 19:51:42 +0200

    Hausaufgabe 17 erledigt

---
+++ src/lesson_17/objetsArray/PersonsApp.java
@@ -0,0 +1,11 @@
+package lesson_17.objetsArray;/* Group: 52-1, "AIT Hi-tech team" GMBH 
+Author: Bogdan Fesenko
+Date: 02-10-2024
+*/
+/*
+
+*/
+public class PersonsApp {
+ public static void main(String[] args) {
+     
+}
