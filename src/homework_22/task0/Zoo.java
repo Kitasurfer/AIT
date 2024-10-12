31a289eb78ecb75ad465cc944eea51c3f96e41e1
BogdanF <sovtour@gmail.com>
2024-10-12 22:10:47 +0200

    lesson 22 add  Animal name

---
+++ src/homework_22/task0/Zoo.java
@@ -0,0 +1,26 @@
+package homework_22.task0;/* Group: 52-1, "AIT Hi-tech team" GMBH 
+Author: Bogdan Fesenko
+Date: 12-10-2024
+*/
+
+import lesson_22.animals.Animal;
+import lesson_22.animals.Cat;
+import lesson_22.animals.Dog;
+import lesson_22.animals.Hamster;
+
+/*
+
+ */
+public class Zoopark  {
+    public static void main(String[] args) {
+
+        Animal[] animals = new Animal[]{new Cat("Barsik"), new Dog("Jack"), new Hamster("Homyak")};
+        for (Animal animal: animals){
+            System.out.println(animal);
+            animal.voice();
+        }
+
+
+    }
+
+}
