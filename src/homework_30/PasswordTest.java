6d3742000e096dd15d30f31b612f3e5689c95a7c
BogdanF <sovtour@gmail.com>
2024-10-23 22:31:22 +0200

    HG_30

--- src/homework_30/PasswordTest.java
+++ src/homework_30/PasswordTest.java
@@ -38,13 +38,13 @@ public class PasswordTest {
 
     }
 
-    // Источник данных для валидных паролей
+    //для валидных паролей
     static Stream<String> validPasswordProvider() {
         return Stream.of(
-                "StrongPass1!",      // Стандартный пароль
-                "Valid123!Abc",      // с разными символами
+                "StrongPass1!", // Стандартный пароль
+                "Valid123!Abc",// с разными символами
                 "Complex$P!a#ssword1", // пароль с символами
-                "Passw0rd@123",      // пароль с цифрами заглавными и специальными символами
+                "Passw0rd@123",// пароль с цифрами заглавными и специальными символами
                 "My$Super123Passw345ordfghf!,"// длинный пароль
         );
 
@@ -62,21 +62,21 @@ public class PasswordTest {
         String previousPassword = person.getPassword(); // Сохраняем старый пароль
         person.setPassword(invalidPassword);
         Assertions.assertNotEquals(invalidPassword, person.getPassword()); // пароль не изменился
-        Assertions.assertEquals(previousPassword, person.getPassword());   // старый пароль остался
+        Assertions.assertEquals(previousPassword, person.getPassword());// старый пароль остался
     }
 
     // для невалидных паролей
     static Stream<String> invalidPasswordProvider() {
         return Stream.of(
-                "short1!",            // короткий пароль
-                "NoSpecial123",       // Нет специального символа
-                "NoDigit!",           // Нет цифры
-                "NOLOWERCASE123!",    // Нет строчных букв
-                "nouppercase123!",    // Нет заглавных букв
+                "short1!",// короткий пароль
+                "NoSpecial123",// Нет специального символа
+                "NoDigit!",// Нет цифры
+                "NOLOWERCASE123!",// Нет строчных букв
+                "nouppercase123!",// Нет заглавных букв
                 "Password with space",//с пробелом
-                "123456789012",       // Только цифры нет букв и специальных символов
-                "passwordpassword",   // Нет цифр и заглавных букв
-                "PASSWORDPASSWORD!",  // Нет цифр и маленьких букв
+                "123456789012",// Только цифры нет букв и специальных символов
+                "passwordpassword", // Нет цифр и заглавных букв
+                "PASSWORDPASSWORD!",// Нет цифр и маленьких букв
                 "abcABC123"// Нет спец символа
         );
 
