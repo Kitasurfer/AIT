4a7b3347f7a7d0e6d802ddbddf9dca48d363de8f
BogdanF <sovtour@gmail.com>
2024-11-05 00:25:22 +0100

    HG_38.

--- src/homework_38/Task1.java
+++ src/homework_38/Task1.java
@@ -9,8 +9,19 @@ import java.util.Comparator;
  * Author: Bogdan Fesenko
  * Date: 04-11-2024
  */
-/*
 
+/**
+ * Task 1
+ * Создайте класс Sportsman с тремя полями: name (имя), age (возраст) и score (рейтинг).
+ * Реализуйте интерфейс Comparable в классе Sportsman, чтобы задать “естественный” порядок сортировки,
+ * например, по name.
+ * Создайте отдельный класс, реализующий Comparator, чтобы сортировать объекты класса Sportsman по другому критерию,
+ * например, по score.
+ * В методе main создайте массив из нескольких объектов Sportsman.
+ * Отсортируйте его с использованием: естественного порядка, определенного в Comparable,
+ * данного класса Comparator,
+ * анонимного класса Comparator для сортировки по третьему полю (например, age).
+ * Выведите каждый вариант отсортированного массива в консоль.
  */
 public class Task1 {
     public static void main(String[] args) {
@@ -35,7 +46,6 @@ public class Task1 {
         Arrays.asList(sportsman).forEach(System.out::println);
 
 
-
         System.out.println("Сортировка по возрасту");
         Arrays.sort(sportsman, new Comparator<Sportsman>() {
             @Override
@@ -48,7 +58,5 @@ public class Task1 {
         System.out.println("\nСортировка по рейтингу");
         Arrays.sort(sportsman, new ScoreComparator());
         Arrays.asList(sportsman).forEach(System.out::println);
-
     }
-
 }
